1e4084a07304658ad26ccbbc71fadaa1a6eae81a
Philip Hickey <phlickey@gmail.com>
2020-05-06 12:34:09 -0400

    Updates

---
+++ precache-manifest.97ebe2e0c70aa370e016cdf359d27e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648dff72e0039c740936f052a5a4aaf0",
+    "url": "/cm-coding-challenge/index.html"
+  },
+  {
+    "revision": "8f8ba56316293c02f7ca",
+    "url": "/cm-coding-challenge/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bfdc03e70ae794ec63b2",
+    "url": "/cm-coding-challenge/static/js/2.55aebaaf.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/cm-coding-challenge/static/js/2.55aebaaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8ba56316293c02f7ca",
+    "url": "/cm-coding-challenge/static/js/main.9bca2006.chunk.js"
+  },
+  {
+    "revision": "2810d5fc561b1e4f900b",
+    "url": "/cm-coding-challenge/static/js/runtime-main.399cf60e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b631b6fbd0e728e020f74bf6a5c9ab91.js"
+  "/cm-coding-challenge/precache-manifest.97ebe2e0c70aa370e016cdf359d27e88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cm-coding-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
